5c39577c3ff13c66fdd79df228b4ccbf22761aa0
Filipe Silva <pg15181@alunos.uminho.pt>
2025-10-31 10:04:16 +0000

    Parse to int values from redis again

--- api/data.js
+++ api/data.js
@@ -21,8 +21,8 @@ export default async function handler(req, res) {
 
     try {
         const audioDataLength = audioData.length;
-        const rootIndex = await parseInt(redis.get("rootIndex"));
-        const currentIndex = await parseInt(redis.get("currentIndex"));
+        const rootIndex = parseInt(await redis.get("rootIndex"));
+        const currentIndex = parseInt(await redis.get("currentIndex"));
         const nextUpIndex = modulo(currentIndex + 1, audioDataLength);
         const voiced = modulo(currentIndex - rootIndex, audioDataLength);
         let result = {};
