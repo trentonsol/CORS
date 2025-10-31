09478ea0c3f05e51028b98985157cf96f0856af1
Filipe Silva <pg15181@alunos.uminho.pt>
2025-10-31 09:38:35 +0000

    Fix typo

--- api/init.js
+++ api/init.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     result.currentIndex = currentIndex;
     
-    result.nexUp = { 
+    result.nextUp = { 
       name: audioData[currentIndex].name, 
       title: audioData[currentIndex].title 
     };
